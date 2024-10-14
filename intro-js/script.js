58cf72f8b8336186cbfe99ffd32b247aa37493f8
Jan M <jan.kiel665@gmail.com>
2024-10-14 09:29:11 +0200

    Day 30 - intro to JavaScript   script, variables,arrays, objects

---
+++ intro-js/script.js
@@ -0,0 +1,21 @@
+/*let greetingText = 'Hi, I\'m Max!!';
+let age = 32;
+alert(greetingText);
+alert(greetingText);
+greetingText = 'I am *really* Max.';
+alert(greetingText);
+alert('My age is ' + age);*/
+
+let userName = 'Max';
+let age = 32;
+let hobbies = ['Sports', 'Cooking', 'Reading'];
+let job = {
+  title: 'Developer',
+  place: 'New York',
+  salary: 50000,
+};
+// alert(hobbies[0]);
+alert(`Hi, my name is ${userName}, I am ${age} years old, I work 
+as ${job.title} in ${job.place} (earning ${job.salary}$!). 
+I love ${hobbies} in my free time.`
+);
