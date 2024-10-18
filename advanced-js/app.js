3ae9a1c3c25a10998b7754eea9f2703189a462d1
Jan M <jan.kiel665@gmail.com>
2024-10-18 23:39:31 +0200

    Day 34 - searching for DOM elements

--- advanced-js/app.js
+++ advanced-js/app.js
@@ -1,1 +1,18 @@
-document.body.children[1].children[0].href = "https://google.com";
+// document.body.children[1].children[0].href = "https://google.com";
+
+// console.dir(document);
+// alert();
+// window.alert();
+
+// document.body.children[0];
+// console.dir(document.body.children[0]);
+// console.log(document.body.children[0]);
+// what is interesting, there is no difference between dir
+// and log output when code in js script
+
+let anchorElement = document.getElementById("external-link");
+anchorElement.href = "https://duckduckgo.com";
+
+// anchorElement = document.querySelector("#external-link");
+anchorElement = document.querySelector("a");
+anchorElement.href = "https://academind.com/";
