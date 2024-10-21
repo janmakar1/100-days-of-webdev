bbb695802ae917f64a315394c9703a6e9bc1ae73
Jan M <jan.kiel665@gmail.com>
2024-10-21 22:37:27 +0200

    Day 37 - exercise with DOM completed and intro to JS conditionals

---
+++ advanced-js/cond.js
@@ -0,0 +1,15 @@
+const name = "Max";
+if (name == "Max") {
+  console.log("Hello Max!");
+} else {
+  console.log("I don't know you!");
+}
+
+console.log(5 !== 5);
+console.log(4 !== 5);
+console.log(5 !== "5");
+console.log(5 != "5");
+console.log(3 > 1);
+console.log(4 > 1);
+console.log("a" > "b");
+console.log("cb" > "ab");
