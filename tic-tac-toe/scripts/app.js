7ba8032ae6a58a98c4128bd1edfca4d6f9b40a12
Jan M <jan.kiel665@gmail.com>
2024-10-26 23:30:25 +0200

    Day 42 - JavaScript scripts for Tic Tac Toe game

--- tic-tac-toe/scripts/app.js
+++ tic-tac-toe/scripts/app.js
@@ -1,12 +1,22 @@
 const playerConfigOverlayElement = document.getElementById("config-overlay");
 const backdropElement = document.getElementById("backdrop");
+const formElement = document.querySelector("form");
+const errorsOutputElement = document.getElementById("config-errors");
 
 const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
 const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
 const cancelConfigBtnElement = document.getElementById("cancel-config-btn");
+const player1PlaceholderElement = document.getElementById(
+  "player1-placeholder"
+);
+const player2PlaceholderElement = document.getElementById(
+  "player2-placeholder"
+);
 
 editPlayer1BtnElement.addEventListener("click", openPlayerConfig);
 editPlayer2BtnElement.addEventListener("click", openPlayerConfig);
 
 cancelConfigBtnElement.addEventListener("click", closePlayerConfig);
 backdropElement.addEventListener("click", closePlayerConfig);
+
+formElement.addEventListener("submit", savePlayerConfig);
--- tic-tac-toe/scripts/config.js
+++ tic-tac-toe/scripts/config.js
@@ -6,4 +6,23 @@ function openPlayerConfig() {
 function closePlayerConfig() {
   playerConfigOverlayElement.style.display = "none";
   backdropElement.style.display = "none";
+  formElement.firstElementChild.classList.remove("error");
+  errorsOutputElement.textContent = "";
+}
+
+function savePlayerConfig(event) {
+  console.log(event);
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  console.log(formData);
+  const enteredPlayername = formData.get("playername").trim();
+  console.log(enteredPlayername);
+
+  if (!enteredPlayername) {
+    event.target.firstElementChild.classList.add("error");
+    errorsOutputElement.textContent = "Please enter a valid name!";
+    return;
+  }
+
+  player1PlaceholderElement.textContent = enteredPlayername;
 }
