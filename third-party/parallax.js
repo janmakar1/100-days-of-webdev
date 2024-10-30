156ccc5c335a36d750c0862278dc137fd84b2ce7
Jan M <jan.kiel665@gmail.com>
2024-10-30 22:13:49 +0100

    Day 46 - Third Party Packages - parallax

---
+++ third-party/parallax.js
@@ -0,0 +1,9 @@
+const imageElement = document.getElementById("main-img");
+new SimpleParallax(imageElement, {
+  scale: 1.6,
+  delay: 0.1,
+});
+// new simpleParallax(imageElement, {
+//   scale: 1.6,
+//   delay: 0.1,
+// });
