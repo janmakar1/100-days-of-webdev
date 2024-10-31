efa767af76ae8ae06b255cfe9defed14fc15db24
Jan M <jan.kiel665@gmail.com>
2024-11-01 00:01:30 +0100

    Day 48 - ExpressJS introduction

--- nodejs-intro/app.js
+++ nodejs-intro/app.js
@@ -1,18 +1,38 @@
-const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
-function handleRequest(request, response) {
-  if (request.url == "/") {
-    response.statusCode = 200;
-    response.end("<h1>Hello World!</h1>");
-  } else if (request.url == "/currenttime") {
-    response.statusCode = 200;
-    response.end("<h1>" + new Date().toISOString() + "</h1>");
-  } else {
-    response.statusCode = 404;
-    response.end(`<h1>Error 404 - ${request.url} - Page not found</h1>`);
-  }
-}
+const express = require("express");
 
-const server = http.createServer(handleRequest);
+const app = express();
 
-server.listen(3000);
+app.use(express.urlencoded());
+
+app.get("/currenttime", function (req, res) {
+  res.send("<h1>" + new Date().toISOString() + "</h1>");
+});
+
+app.get("/", function (req, res) {
+  res.send(
+    '<form action="/store-user" method="POST"><label>Your Name</label>' +
+      '<input type="text" name="username" /><button>Ok!</button></form>'
+  );
+});
+
+app.post("/store-user", function (req, res) {
+  const userName = req.body.username;
+  console.log(userName);
+
+  const filePath = path.join(__dirname, "data", "users.json");
+
+  console.log(__dirname);
+  console.log(filePath);
+
+  const fileData = fs.readFileSync(filePath);
+  const existingUsers = JSON.parse(fileData);
+  existingUsers.push(userName);
+  fs.writeFileSync(filePath, JSON.stringify(existingUsers));
+
+  res.send(`<h1>Username ${userName} saved into database.</h1>`);
+});
+
+app.listen(3000);
