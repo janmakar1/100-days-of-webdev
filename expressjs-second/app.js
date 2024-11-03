868f421f2c70780019202389ab242163b813427a
Jan M <jan.kiel665@gmail.com>
2024-11-03 21:41:10 +0100

    Day 50 - Resturants ExpressJS app - EJS interpolation, loops, conditionals

--- expressjs-second/app.js
+++ expressjs-second/app.js
@@ -1,34 +1,54 @@
+const fs = require("fs");
 const path = require("path");
 
 const express = require("express");
 
 const app = express();
 
-const htmlDirectoryPath = path.join(__dirname, "views");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/about", function (req, res) {
-  const htmlFilePath = path.join(htmlDirectoryPath, "about.html");
-  res.sendFile(htmlFilePath);
+  res.render("about");
 });
 
 app.get("/confirm", function (req, res) {
-  const htmlFilePath = path.join(htmlDirectoryPath, "confirm.html");
-  res.sendFile(htmlFilePath);
+  res.render("confirm");
 });
 
 app.get("/", function (req, res) {
-  const htmlFilePath = path.join(htmlDirectoryPath, "index.html");
-  res.sendFile(htmlFilePath);
+  res.render("index");
 });
 
 app.get("/recommend", function (req, res) {
-  const htmlFilePath = path.join(htmlDirectoryPath, "recommend.html");
-  res.sendFile(htmlFilePath);
+  res.render("recommend");
+});
+
+app.post("/recommend", function (req, res) {
+  const restaurant = req.body;
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+  storedRestaurants.push(restaurant);
+
+  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+
+  res.redirect("/confirm");
 });
 
 app.get("/restaurants", function (req, res) {
-  const htmlFilePath = path.join(htmlDirectoryPath, "restaurants.html");
-  res.sendFile(htmlFilePath);
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+  res.render("restaurants", {
+    numberOfRestaurants: storedRestaurants.length,
+    restaurants: storedRestaurants,
+  });
 });
 
 app.listen(3000);
