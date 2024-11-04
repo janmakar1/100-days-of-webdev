d388ec1f1e37e8c57157dd8a5141dd3631da206b
Jan M <jan.kiel665@gmail.com>
2024-11-04 13:35:21 +0100

    Day 51 - Restaurants app - partials, dynamic routes

--- expressjs-second/app.js
+++ expressjs-second/app.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 const express = require("express");
+const uuid = require("uuid");
 
 const app = express();
 
@@ -29,6 +30,7 @@ app.get("/recommend", function (req, res) {
 
 app.post("/recommend", function (req, res) {
   const restaurant = req.body;
+  restaurant.id = uuid.v4();
   const filePath = path.join(__dirname, "data", "restaurants.json");
 
   const fileData = fs.readFileSync(filePath);
@@ -49,6 +51,11 @@ app.get("/restaurants", function (req, res) {
     numberOfRestaurants: storedRestaurants.length,
     restaurants: storedRestaurants,
   });
+});
+
+app.get("/restaurants/:id", function (req, res) {
+  const restaurantId = req.params.id;
+  res.render("restaurant-detail", { rid: restaurantId });
 });
 
 app.listen(3000);
