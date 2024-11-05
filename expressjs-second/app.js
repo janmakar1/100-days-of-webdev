823fc3e659f964606bea4162c9378ba7418a973e
Jan M <jan.kiel665@gmail.com>
2024-11-06 00:18:22 +0100

    Day 53 - Restaurant ExpressJS app finished (router split)

--- expressjs-second/app.js
+++ expressjs-second/app.js
@@ -1,10 +1,9 @@
-const fs = require("fs");
 const path = require("path");
 
 const express = require("express");
-const uuid = require("uuid");
 
-const resData = require("./util/restaurant-data");
+const defaultRoutes = require("./routes/default");
+const restaurantsRoutes = require("./routes/restaurants");
 
 const app = express();
 
@@ -14,54 +13,8 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/about", function (req, res) {
-  res.render("about");
-});
-
-app.get("/confirm", function (req, res) {
-  res.render("confirm");
-});
-
-app.get("/", function (req, res) {
-  res.render("index");
-});
-
-app.get("/recommend", function (req, res) {
-  res.render("recommend");
-});
-
-app.post("/recommend", function (req, res) {
-  const restaurant = req.body;
-  restaurant.id = uuid.v4();
-  const restaurants = resData.getStoredRestaurants();
-  restaurants.push(restaurant);
-
-  resData.storeRestaurants(restaurants);
-
-  res.redirect("/confirm");
-});
-
-app.get("/restaurants", function (req, res) {
-  const storedRestaurants = resData.getStoredRestaurants();
-  res.render("restaurants", {
-    numberOfRestaurants: storedRestaurants.length,
-    restaurants: storedRestaurants,
-  });
-});
-
-app.get("/restaurants/:id", function (req, res) {
-  const restaurantId = req.params.id;
-  const storedRestaurants = resData.getStoredRestaurants();
-  for (const restaurant of storedRestaurants) {
-    if (restaurant.id === restaurantId) {
-      res.render("restaurant-detail", {
-        restaurant: restaurant,
-      });
-    }
-  }
-
-  return res.status(404).render("404");
-});
+app.use("/", defaultRoutes);
+app.use("/", restaurantsRoutes);
 
 app.use(function (req, res) {
   res.status(404).render("404");
