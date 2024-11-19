387d86328b04fa65ee6e33a9130cc6e52738ea07
Jan M <jan.kiel665@gmail.com>
2024-11-19 21:54:37 +0100

    Day 64 - MongoDB blog finished

--- blog-mongodb/routes/blog.js
+++ blog-mongodb/routes/blog.js
@@ -11,8 +11,14 @@ router.get("/", function (req, res) {
   res.redirect("/posts");
 });
 
-router.get("/posts", function (req, res) {
-  res.render("posts-list");
+router.get("/posts", async function (req, res) {
+  const posts = await db
+    .getDb()
+    .collection("posts")
+    .find({})
+    .project({ title: 1, summary: 1, 'author.name': 1})
+    .toArray();
+  res.render("posts-list", { posts: posts });
 });
 
 router.post("/posts", async function (req, res) {
@@ -29,7 +35,7 @@ router.post("/posts", async function (req, res) {
     date: new Date(),
     author: {
       id: authorId,
-      name: author,
+      name: author.name,
       email: author.email,
     },
   };
@@ -38,6 +44,66 @@ router.post("/posts", async function (req, res) {
   console.log(result);
   res.redirect("/posts");
 });
+
+router.get("/posts/:id", async function (req, res) {
+  const authorId = new ObjectId(req.body.author);
+  const postId = req.params.id;
+  const post = await db.getDb().collection("posts").findOne({_id: new ObjectId(postId)});
+
+  if (!post) {
+    return res.status(404).render("404");
+  }
+
+  post.humanReadableDate = post.date.toLocaleDateString("en-US", {
+    weekday: "long",
+    year:"numeric",
+    month: "long",
+    day: "numeric",
+  });
+  post.date = post.date.toISOString();
+  res.render("post-detail", { post: post });
+});
+
+router.get("/posts/:id/edit", async function (req, res) {
+  const authorId = new ObjectId(req.body.author);
+  const postId = req.params.id;
+  const post = await db
+    .getDb()
+    .collection("posts")
+    .findOne({_id: new ObjectId(postId)});
+
+  if (!post) {
+    return res.status(404).render("404");
+  }
+
+  res.render("update-post", { post: post });
+});
+
+router.post("/posts/:id/edit", async function(req, res) {
+  const postId = new ObjectId(req.params.id);
+  await db.getDb()
+    .collection("posts")
+    .updateOne(
+      {_id: postId},
+      {
+        $set: {
+          title: req.body.title,
+          summary: req.body.summary,
+          body: req.body.content,
+        },
+      }
+    );
+
+  res.redirect("/posts");
+
+});
+
+router.post("/posts/:id/delete", async function(req, res) {
+  const postId = new ObjectId(req.params.id);
+  db.getDb().collection("posts").deleteOne({_id: postId});
+  res.redirect("/posts");
+})
+
 
 router.get("/new-post", async function (req, res) {
   const authors = await db.getDb().collection("authors").find().toArray();
