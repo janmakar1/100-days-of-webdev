2fc64541a40d8fd95a56e4bac741fca0a58fe241
Jan M <jan.kiel665@gmail.com>
2024-12-06 23:07:01 +0100

    Day 83 - Shop Project - authentication, header

--- shop-mega-project/controllers/auth.controller.js
+++ shop-mega-project/controllers/auth.controller.js
@@ -1,12 +1,62 @@
 const User = require("../models/user.model");
 const authUtil = require("../util/authentication");
 const validation = require("../util/validation");
+const sessionFlash = require("../util/session-flash");
 
 function getSignup(req, res) {
-  res.render("customer/auth/signup");
+  let sessionData = sessionFlash.getSessionData(req);
+
+  if (!sessionData) {
+    sessionData = {
+      email: "",
+      confirmEmail: "",
+      password: "",
+      fullname: "",
+      street: "",
+      postal: "",
+      city: "",
+    };
+  }
+
+  res.render("customer/auth/signup", { inputData: sessionData });
 }
 
 async function signup(req, res) {
+  const enteredData = {
+    email: req.body.email,
+    confirmEmail: req.body["confirm-email"],
+    password: req.body.password,
+    fullname: req.body.fullname,
+    street: req.body.street,
+    postal: req.body.postal,
+    city: req.body.city,
+  };
+
+  if (
+    !validation.userDetailsAreValid(
+      req.body.email,
+      req.body.password,
+      req.body.fullname,
+      req.body.street,
+      req.body.postal,
+      req.body.city
+    ) ||
+    !validation.emailIsConfirmed(req.body.email, req.body["confirm-email"])
+  ) {
+    sessionFlash.flashDataToSession(
+      req,
+      {
+        errorMessage:
+          "Please check your input. Password must be at least 5 characters long.",
+        ...enteredData,
+      },
+      function () {
+        res.redirect("/signup");
+      }
+    );
+    return;
+  }
+
   const user = new User(
     req.body.email,
     req.body.password,
@@ -16,25 +66,19 @@ async function signup(req, res) {
     req.body.city
   );
 
-  if (
-    !validation.userDetailsAreValid(
-      user.email,
-      user.password,
-      user.name,
-      user.street,
-      user.postal,
-      user.city
-    ) ||
-    !validation.emailIsConfirmed(req.body.email, req.body["confirm-email"])
-  ) {
-    res.redirect("/signup");
-    return;
-  }
-
   try {
     const existsAlready = await user.existsAlready();
     if (existsAlready) {
-      res.redirect("/signup");
+      sessionFlash.flashDataToSession(
+        req,
+        {
+          errorMessage: "User exists already! Try logging in instead!",
+          ...enteredData,
+        },
+        function () {
+          res.redirect("/signup");
+        }
+      );
       return;
     }
 
@@ -48,7 +92,15 @@ async function signup(req, res) {
 }
 
 function getLogin(req, res) {
-  res.render("customer/auth/login");
+  let sessionData = sessionFlash.getSessionData(req);
+  if (!sessionData) {
+    sessionData = {
+      email: "",
+      password: "",
+    };
+  }
+
+  res.render("customer/auth/login", { inputData: sessionData });
 }
 
 async function login(req, res, next) {
@@ -61,8 +113,17 @@ async function login(req, res, next) {
     return;
   }
 
+  const sessionErrorData = {
+    errorMessage:
+      "Invalid credentials - please double check your email and password!",
+    email: user.email,
+    password: user.password,
+  };
+
   if (!existingUser) {
-    res.redirect("/login");
+    sessionFlash.flashDataToSession(req, sessionErrorData, function () {
+      res.redirect("/login");
+    });
     return;
   }
 
@@ -71,7 +132,9 @@ async function login(req, res, next) {
   );
 
   if (!passwordIsCorrect) {
-    res.redirect("/login");
+    sessionFlash.flashDataToSession(req, sessionErrorData, function () {
+      res.redirect("/login");
+    });
     return;
   }
 
