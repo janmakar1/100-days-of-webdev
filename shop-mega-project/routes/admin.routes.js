07bbfc65124dd7b4a8fd80273e61240df89c23a8
Jan M <jan.kiel665@gmail.com>
2024-12-08 22:10:03 +0100

    Day 85 - Shop Project - products and add new products pages styling incl. mobile view

---
+++ shop-mega-project/routes/admin.routes.js
@@ -0,0 +1,10 @@
+const adminController = require("../controllers/admin.controller");
+
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/products", adminController.getProducts);
+router.get("/products/new", adminController.getNewProduct);
+
+module.exports = router;
