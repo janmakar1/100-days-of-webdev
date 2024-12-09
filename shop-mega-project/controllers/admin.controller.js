a2245832d15098b47ce7f221ece35119b8643b62
Jan M <jan.kiel665@gmail.com>
2024-12-09 23:16:15 +0100

    Day 86 - adding new product, uploading product image and showing all products

--- shop-mega-project/controllers/admin.controller.js
+++ shop-mega-project/controllers/admin.controller.js
@@ -1,12 +1,37 @@
-function getProducts(req, res) {
-  res.render("admin/products/all-products");
+const Product = require("../models/product.model");
+
+async function getProducts(req, res) {
+  try {
+    const products = await Product.findAll();
+    res.render("admin/products/all-products", { products: products });
+  } catch (error) {
+    next(error);
+    return;
+  }
 }
 
 function getNewProduct(req, res) {
   res.render("admin/products/new-product");
 }
 
-function createNewProduct() {}
+function createNewProduct(req, res) {
+  console.log(req.body);
+  console.log(req.file);
+
+  const product = new Product({
+    ...req.body,
+    image: req.file.filename,
+  });
+
+  try {
+    product.save();
+  } catch (error) {
+    next(error);
+    return;
+  }
+
+  res.redirect("/admin/products");
+}
 
 module.exports = {
   getProducts: getProducts,
